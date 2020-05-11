285c1c8aee0810b41701c8da7ce53b1338a624f7
Josh Drummond <josh.drummond106@gmail.com>
2020-05-11 16:49:56 +1000

    Updated project files and notes

---
+++ repos/proj/docs/project_notes.R
@@ -0,0 +1,14 @@
+Questions to ask Dr Mara:
+  
+1. How to include a caption over multiple lines, rather than it continuous 
+2. 
+
+
+NOTES:
+When attempting to judge how good a team will be in the standings, offensive rating is key. The most successful teams in 2018-19 all had stellar offensive ratings. Defensive rating, while not as important as its offensive counterpart, can also provide a strong indication of a team’s W-L record. These all-encompassing numbers will usually dictate how a team’s season will turn out.
+
+Efficiency matters more than attempts for both overall shots and 3-point shots. The value of a 3-point attempt is more than a regular field goal attempt and shooting a better percentage from behind the arc means more in today’s NBA.
+
+Rebounding differential is more important than a strong assist/turnover ratio. The margin in the latter statistic might be too small to create a difference in the standings. Dominate the boards, however, and you’re more likely to see a positive impact. Pace is largely irrelevant.
+
+But at the end of the season, these stats all contribute to the two numbers that matter most: wins and losses.
