97374a598515deaae917b73320fb6afdbdd5609e
Josh Drummond <josh.drummond106@gmail.com>
2020-05-18 12:13:21 +1000

    Updated files

--- repos/proj/docs/project_notes.R
+++ repos/proj/docs/project_notes.R
@@ -1,14 +1,53 @@
 Questions to ask Dr Mara:
   
-1. How to include a caption over multiple lines, rather than it continuous 
-2. 
-
+1. Players with two listed positions. How to split the column and keep a position?
+3. 
+4. 
+5. 
 
 NOTES:
-When attempting to judge how good a team will be in the standings, offensive rating is key. The most successful teams in 2018-19 all had stellar offensive ratings. Defensive rating, while not as important as its offensive counterpart, can also provide a strong indication of a team’s W-L record. These all-encompassing numbers will usually dictate how a team’s season will turn out.
-
+When attempting to judge how good a team will be in the standings, offensive rating is key. 
+The most successful teams in 2018-19 all had stellar offensive ratings. Defensive rating, while not as important as its offensive counterpart, can also provide a strong indication of a team’s W-L record. 
+These all-encompassing numbers will usually dictate how a team’s season will turn out.
 Efficiency matters more than attempts for both overall shots and 3-point shots. The value of a 3-point attempt is more than a regular field goal attempt and shooting a better percentage from behind the arc means more in today’s NBA.
-
 Rebounding differential is more important than a strong assist/turnover ratio. The margin in the latter statistic might be too small to create a difference in the standings. Dominate the boards, however, and you’re more likely to see a positive impact. Pace is largely irrelevant.
-
 But at the end of the season, these stats all contribute to the two numbers that matter most: wins and losses.
+
+
+GO BACK AND FIX IN TIDY DATA
+1. If required to remove players secondary position. (Create new column or remove and replace with a single position_dodge)
+
+
+NEXT ON TO DO LIST:
+SINGLE LINEAR REGRESSION:
+  
+ggplot
+correlation
+linear regression model for position specific analysis
+predict values
+detect outliers - hat values - cooks distance - dealing with any?
+Durbin Watson test
+Homoscedasticity
+
+
+MULTIPLE LINEAR REGRESSION:
+
+Confounding variables using pairs
+conf.int model
+predict using above model
+linearity using avplots
+multicollineariy using variance inflation factor
+
+Create the expected points, assists and rebounds based on if all players are exactly the same. Must be position specific though
+Then select player according to their value on salary 
+
+CREATE FINAL GRAPHS with labels and accurate headings
+
+Write up report explaining the final findings
+
+Organise code for each step in particular file
+
+Upload to github 
+
+
+  
